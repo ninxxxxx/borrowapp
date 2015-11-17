3f7c66693f83f58aeab215b17249af7d0997774e
anchansi <pomduck-dumit@hotmail.com>
2015-11-17 21:42:20 +0700

    bootstrap update เพิ่ม ข้อมูลเบิกกับยืม กับ จำนวนuser และ item

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -15,42 +15,68 @@ class BootStrap {
         def user1 = new User(firstName:"pang", lastName:"DDk", username:"pangpang", password:"123456", tel:"0850680467", passcode:"5465", rfidCode:"98798784654")
         def user2 = new User(firstName:"anchan", lastName:"si", username:"chunchun", password:"987654", tel:"0850680545", passcode:"3333", rfidCode:"9987545654")
         def user3 = new User(firstName:"pungpond", lastName:"dekdek", username:"dekdek", password:"987987", tel:"08587840545", passcode:"3543", rfidCode:"99898795654")
+        def user4 = new User(firstName:"prapass", lastName:"siri", username:"poppop", password:"111111", tel:"08587840544", passcode:"0000", rfidCode:"99898796654")
+        def user5 = new User(firstName:"cookie", lastName:"cream", username:"cookies", password:"987604", tel:"0850600545", passcode:"3133", rfidCode:"9987545650")
+        def user6 = new User(firstName:"yok", lastName:"green", username:"yokyok", password:"985654", tel:"0810680545", passcode:"3433", rfidCode:"9957545654")
+        def user7 = new User(firstName:"Prae", lastName:"Lalitwadee", username:"praeprae", password:"987634", tel:"0850210545", passcode:"5533", rfidCode:"9987545634")
+        def user8 = new User(firstName:"arnon", lastName:"arnon", username:"arnonarnon", password:"981654", tel:"0820680545", passcode:"3033", rfidCode:"1957545654")
+
 
         def i1 = new Item(title:"ปากกา ตราม้า", category:"เครื่องเขียน", least:10, amount:100)
         def i2 = new Item(title:"กระดาษA4", category:"กระดาษ", least:10, amount:1000)
         def i3 = new Item(title:"ชอร์ค(กล่อง)", category:"เครื่องเขียน", least:2, amount:25)
-
-        def p1 = new Picking(user:'ooi',item:'pen',isAccept:true)
-        def p2 = new Picking(user:'noi',item:'rubber',isAccept:true)
-        def p3 = new Picking(user:'soi',item:'paper',isAccept:true)
+        def i4 = new Item(title:"ปากกาหมึกซึม", category:"เครื่องเขียน", least:8, amount:25)
+        def i5 = new Item(title:"ปากกาลูกลื่น", category:"เครื่องเขียน", least:8, amount:24)
+        def i6 = new Item(title:"แปรงลบกระดาน", category:"เครื่องเขียน", least:2, amount:15)
+        def i7 = new Item(title:"ถุงขยะ", category:"เบ็ดเตล็ด", least:8, amount:30)
+        def i8 = new Item(title:"ที่โกย", category:"เบ็ดเตล็ด", least:2, amount:8)
+        def i9 = new Item(title:"ถุงมือยาง", category:"เบ็ดเตล็ด", least:8, amount:30)
         
-        def b1 = new Borrowing(item:'pencil',user:'yok')
-        def b2 = new Borrowing(item:'pen',user:'cream')
-        def b3 = new Borrowing(item:'maitu',user:'natt')
         
         admin.save(failOnError: true)
         user1.save(failOnError: true)
         user2.save(failOnError: true)
         user3.save(failOnError: true)
+        user4.save(failOnError: true)
+        user5.save(failOnError: true)
+        user6.save(failOnError: true)
+        user7.save(failOnError: true)
+        user8.save(failOnError: true)
+
         def ur1 = new UserRole(user:admin, role:adminRole).save()
         def ur2 = new UserRole(user:user1, role:adminRole).save()
         def ur3 = new UserRole(user:user2, role:userRole).save()
         def ur4 = new UserRole(user:user3, role:userRole).save()
+        def ur5 = new UserRole(user:user4, role:userRole).save()
+        def ur6 = new UserRole(user:user5, role:userRole).save()
+        def ur7 = new UserRole(user:user6, role:userRole).save()
+        def ur8 = new UserRole(user:user7, role:userRole).save()
+        def ur9 = new UserRole(user:user8, role:userRole).save()
 
 
         i1.save()
         i2.save()
         i3.save()
+        i4.save()
+        i5.save()
+        i6.save()
+        i7.save()
+        i8.save()
+        i9.save()
+
+        def p1 = new Picking(user:user4,item:i1,isAccept:true).save()
+        def p2 = new Picking(user:user2,item:i2,isAccept:true).save()
+        def p3 = new Picking(user:user3,item:i3,isAccept:true).save()
+        def p4 = new Picking(user:user5,item:i4,isAccept:true).save()
+        def p5 = new Picking(user:user6,item:i3,isAccept:true).save()
         
 
-        p1.save()
-        p2.save()
-        p3.save()
+        def b1 = new Borrowing(item:i5,user:user7).save()
+        def b2 = new Borrowing(item:i6,user:user8).save()
+        def b3 = new Borrowing(item:i7,user:user5).save()
 
 
-        b1.save()
-        b2.save()
-        b3.save()
+        
     }
     def destroy = {
     }
